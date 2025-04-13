5b9ffa90568062356f110fae1e6676388f10d231
caleb <brick.cuppy@gmail.com>
2025-04-12 19:30:50 -0700

    Trying out new date format

--- src/server.cpp
+++ src/server.cpp
@@ -89,7 +89,7 @@ vector<unordered_map<string, string>> Server::list_movies(ResultSet* result) {
 			{"poster_path", result->getString(6)},
 			{"tmdb_score", result->getString(7)},
 			{"user_avg_score", result->getString(8)},
-			{"release_date", result->getString(9)}
+			{"release_date", result->getDate(9).toText()}
 		};
 		data.push_back(entry);
 	}
